5e2f5da5825337eb8dfba420333d228a5d6afbe9
Priyanshu1999 <40426639+Priyanshu1999@users.noreply.github.com>
2022-08-25 11:16:15 +0530

    Update boardview.dart
    fixed warning from flutter 3.
    Warning: Operand of null-aware operation '!' has type 'WidgetsBinding' which excludes null.

--- lib/boardview.dart
+++ lib/boardview.dart
@@ -304,7 +304,7 @@ class BoardViewState extends State<BoardView> with AutomaticKeepAliveClientMixin
     print("topListY:${topListY}");
     print("bottomListY:${bottomListY}");
     if(boardViewController.hasClients) {
-      WidgetsBinding.instance!.addPostFrameCallback((Duration duration) {
+      WidgetsBinding.instance.addPostFrameCallback((Duration duration) {
         try {
           boardViewController.position.didUpdateScrollPositionBy(0);
         }catch(e){}
